381c1f16fe1c1b185c428e0fc248581c341eaf43
bother7 <github email address>
2017-07-23 15:41:49 -0400

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
+
 class Triangle
   # write code here
+  attr_reader :first, :second, :third
+
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+
+  end
+
+
+  def kind
+
+    if first <= 0 || second <= 0 || third <= 0
+      # begin
+        raise TriangleError
+      # rescue TriangleError => error
+          # puts error.message
+      # end
+    elsif (first + second) <= third || (second + third) <= first || (first + third) <= second
+      raise TriangleError
+    elsif first == second && second == third
+        :equilateral
+    elsif first == second && first != third
+      :isosceles
+    elsif second == third && first != third
+      :isosceles
+    elsif first == third && second != third
+      :isosceles
+    elsif first != second && second != third && first != third
+      :scalene
+
+    end
+  end
+
+
+
+end
+
+
+
+class TriangleError < StandardError
+  def message
+    "Triangle is illegal"
+  end
 end
